cd332426c22ffa4b280d42b5c8877e3395e22922
Ciaran Archer <ciaran.archer@gmail.com>
2012-12-16 20:29:59 +0000

    Added heading with padding.

--- lib/hacker_term.rb
+++ lib/hacker_term.rb
@@ -23,19 +23,31 @@ module HackerTerm
     def initialize(config={})
       if can_change_color?
         start_color
+        # foreground / background colours
         init_pair(0, COLOR_WHITE, COLOR_BLACK)
         init_pair(1, COLOR_WHITE, COLOR_BLUE)
         init_pair(2, COLOR_WHITE, COLOR_RED)
       end
 
       @column_headers = config[:column_headers]
+      @total_width = cols
+      @total_height = lines
+      @padding_left = 2
+      @buff = []
+
+    end
+
+    def output_line(line_num, data)
+      setpos line_num, 0
+      padding_right = @total_width - data.length - @padding_left
+      addstr((" " * @padding_left) + data + (" " * padding_right))
     end
 
     def draw_header(col_headers)
       attrset(color_pair(1))
       setpos(0, 0)
-      addstr "HACKER NEWS (Thanks to http://hndroidapi.appspot.com/)"
-      addstr("\nArrow keys to select | Enter to open | F5 to refresh")
+      output_line(0, "HACKER NEWS (Thanks to http://hndroidapi.appspot.com/)") 
+      output_line(1, "Arrow keys to select | Enter to open | F5 to refresh")
     end
 
     def draw_footer(sorted_by, stats, key_options)
