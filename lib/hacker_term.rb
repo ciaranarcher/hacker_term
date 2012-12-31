4c2458fc396cbe1ee8f45aa2d4253fb762f6f709
Ciaran Archer <ciaran.archer@gmail.com>
2012-12-31 13:25:03 +0000

    Hooked up to real data!

--- lib/hacker_term.rb
+++ lib/hacker_term.rb
@@ -1,10 +1,11 @@
 require 'hacker_term/page_data'
 require 'hacker_term/ui'
+require 'rest_client'
 
 module HackerTerm
   class TerminalApp
     def initialize
-      @raw_json = File.read './data/data.json'
+      @raw_json = read_json
       @page = PageData.new @raw_json
       @ui = UI.new
     end
@@ -37,6 +38,21 @@ module HackerTerm
     end
 
     private
+
+    def read_json
+      local_proxy = get_local_proxy
+      RestClient.proxy = local_proxy unless local_proxy.empty?
+      RestClient.get 'http://hndroidapi.appspot.com/news/format/json/page/'
+    end
+
+    def get_local_proxy
+      # Cater for both upper and lower case env variables
+
+      local_proxy = `echo $http_proxy`.chomp
+      return local_proxy unless local_proxy.empty?
+
+      `echo $HTTP_PROXY`.chomp
+    end
 
     def clear_and_show
       @ui.clear!
