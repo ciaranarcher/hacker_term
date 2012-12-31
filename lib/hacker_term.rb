14347b6c6696d20a64d26e540a0051e6aaec7946
Ciaran Archer <ciaran.archer@gmail.com>
2012-12-31 11:55:53 +0000

    Added class instance for application.

--- lib/hacker_term.rb
+++ lib/hacker_term.rb
@@ -3,36 +3,44 @@ require 'hacker_term/ui'
 
 module HackerTerm
   class TerminalApp
-    def self.run
+    def initialize
+      @raw_json = File.read './data/data.json'
+      @page = PageData.new @raw_json
+      @ui = UI.new
+    end
 
-
+    def run
       #$stdout.reopen('/dev/null', 'w') # Not sure if I'm going to need this yet...
-      page = PageData.new File.read './data/data.json' 
-      ui = UI.new
-      ui.clear!
-      ui.show page
+      
+      clear_and_show
 
       begin
-        char = ui.get_char
+        char = @ui.get_char
 
         case char.to_s.upcase.chomp
         when "27" # Esc
-          ui.close
+          @ui.close
           break
         when "S"
-          page.sort_on!(:score)
+          @page.sort_on!(:score)
         when "R"
-          page.sort_on!(:rank)
+          @page.sort_on!(:rank)
         when "T"
-          page.sort_on!(:title)
+          @page.sort_on!(:title)
         when "C"
-          page.sort_on!(:comments)
+          @page.sort_on!(:comments)
         end
 
-        ui.clear!
-        ui.show page
+       clear_and_show
 
       end while true
+    end
+
+    private
+
+    def clear_and_show
+      @ui.clear!
+      @ui.show @page
     end
   end
 end
--- run.rb
+++ run.rb
@@ -1,4 +1,5 @@
 $:.unshift(File.dirname(__FILE__) + '/lib')
 
 require 'hacker_term'
-HackerTerm::TerminalApp.run
+app = HackerTerm::TerminalApp.new
+app.run
