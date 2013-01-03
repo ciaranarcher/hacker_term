ccad021eb068648424cfd6e1388dd65b77ef6bd0
Ciaran Archer <ciaran.archer@gmail.com>
2013-01-03 21:31:10 +0000

    Using heredoc for some sample JSON

--- lib/hacker_term/comments_data.rb
+++ lib/hacker_term/comments_data.rb
@@ -9,5 +9,20 @@ module HackerTerm
       unescaped = CGI.unescapeHTML data
       @data = JSON.parse(unescaped)['items']
     end
+
+    def data_as_text(max_width)
+      @data.each do |line|
+         # words = line['comment'].split ' '
+         # new_line = ''
+         # words.inject(0) do |length, word| 
+         #  length + word.length + 1 
+         #  wor
+         #  if length >= max_width
+         #    length = 0 
+
+         #  end
+         # end
+      end
+    end
   end
 end
--- spec/comments_data_spec.rb
+++ spec/comments_data_spec.rb
@@ -2,11 +2,12 @@ require 'hacker_term/comments_data'
 
 module HackerTerm
   describe CommentsData do
-    describe 'formatting' do
+    describe 'format JSON' do
+      let(:many_comments) { File.read './data/comments.json' }
+      let(:lorem) { File.read './data/lorem.txt'}
 
       it 'creates an array of comments' do
-        comments = CommentsData.new File.read './data/comments.json'
-        p comments.data
+        comments = CommentsData.new many_comments
         comments.data.should be_instance_of Array
       end
 
@@ -29,6 +30,28 @@ module HackerTerm
 
         comments = CommentsData.new data
         comments.data.first['comment'].should_not match /&amp;/ 
+      end
+
+      it 'splits a long line into fixed width lines preserving words' do
+        data = %{
+          {
+            "items":[
+              {
+                "username":"tghw",
+                "comment":"#{lorem}",
+                "id":"5003378",
+                "grayedOutPercent":0,
+                "reply_id":"5003378&amp;whence=%69%74%65%6d%3f%69%64%3d%35%30%30%32%39%37%34",
+                "time":"3 hours ago",
+                "children":[
+
+                ]
+              }
+            ]
+          }
+        }
+        comments = CommentsData.new data
+        p comments.data
       end
     end
   end
