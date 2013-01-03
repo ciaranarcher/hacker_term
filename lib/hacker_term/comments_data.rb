935ea724dd89a634db9f00b111f4bf99e9c94247
Ciaran Archer <ciaran.archer@gmail.com>
2013-01-03 20:55:27 +0000

    Unescaping HTML in comments.

--- lib/hacker_term/comments_data.rb
+++ lib/hacker_term/comments_data.rb
@@ -1,11 +1,13 @@
 require 'json'
+require 'cgi'
 
 module HackerTerm
   class CommentsData
     attr_reader :data
 
     def initialize(data)
-      @data = JSON.parse data
+      unescaped = CGI.unescapeHTML data
+      @data = JSON.parse(unescaped)['items']
     end
   end
 end
