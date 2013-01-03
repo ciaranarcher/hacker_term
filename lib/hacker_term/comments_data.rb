17592934dbe2e1c412049422c9ec0af609075deb
Ciaran Archer <ciaran.archer@gmail.com>
2013-01-03 20:44:50 +0000

    Taking comments data over from other branch.

--- lib/hacker_term/comments_data.rb
+++ lib/hacker_term/comments_data.rb
@@ -1,13 +1,20 @@
 require 'json'
+<<<<<<< HEAD
 require 'cgi'
+=======
+>>>>>>> 7e1ec95... Parsing comments data
 
 module HackerTerm
   class CommentsData
     attr_reader :data
 
     def initialize(data)
+<<<<<<< HEAD
       unescaped = CGI.unescapeHTML data
       @data = JSON.parse(unescaped)['items']
+=======
+      @data = JSON.parse data
+>>>>>>> 7e1ec95... Parsing comments data
     end
   end
 end
